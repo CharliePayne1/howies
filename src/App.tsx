47136fbfffbf9dc83ccbef734fa50499f69aa1dc
Charlie Payne <charliepayne1101@gmai.com>
2024-06-23 22:12:33 +0100

    add initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Logo from './logo.png';
+import hero1 from './hero1.jpg';
+import hero2 from './hero2.jpg';
+import hero3 from './hero3.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="relative min-h-screen m-16">
+      <div className="flex justify-end absolute top-0 right-0">
+        <a href="mailto:james@howiescider.com" target="_blank" rel="noopener noreferrer"> {/* Email link */}
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+          </svg>
         </a>
-      </header>
+        <a href="https://instagram.com/howiescider/" target="_blank" rel="noopener noreferrer"> {/* Instagram link */}
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+          </svg>
+        </a>
+      </div>
+
+      <div className="flex flex-col items-center mt-16"> {/* Added margin-top for spacing */}
+        <img src={Logo} alt="Howie's Cider Logo" className="w-80 mb-8" />
+        <div className="flex justify-center w-full space-x-8">
+          <img src={hero1} alt="hero1" className="w-1/3" />
+          <img src={hero2} alt="hero2" className="w-1/3" />
+          <img src={hero3} alt="hero3" className="w-1/3" />
+        </div>
+      </div>
     </div>
   );
 }
