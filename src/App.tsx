9a3e4c330ed29bc66d9dff48ae14b0ec900039c9
Charlie Payne <charliepayne1101@gmai.com>
2024-06-27 13:00:00 +0200

    chnage icons

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,20 @@ import hero3 from './hero3.jpg';
 function App() {
   return (
     <div className="relative min-h-screen m-16 mt-4">
-      <div className="flex justify-end -mr-12">
-        <a href="mailto:james@howiescider.com" target="_blank" rel="noopener noreferrer">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-          </svg>
-        </a>
-        <a href="https://instagram.com/howiescider/" target="_blank" rel="noopener noreferrer"> {/* Instagram link */}
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
-          </svg>
-        </a>
+      <div className="flex justify-between -mr-12">
+        <div className='font-bold'>
+          <a href="https://www.amazon.co.uk/Howies-Fruit-Cider-Flavour-Bottles/dp/B0CZ9J7QKM/ref=sr_1_5?crid=OK2TTRLDM7ET&dib=eyJ2IjoiMSJ9.4DGkgdHO2Am6GDC0FhHS6fxCfZVHqRHAtlQa7VYAUuDjHNKX2Cxn5desHV48gm1OCbMkrOxUGKl6IazVO2kPLUBx4-qv8mJYWXrkdbwBW9pIftLhjSKoLIHlNGTNY_vW3kb4OHa31G9fRMdj1SLEQiCVZMokLuRUSpisUX3hgxNFYkpNKkXwVNZtni5rWwt0aUi6W114qsQHUuV-g_a1H6BPax0ZvaVRAWHNELe5LPfYAsIeJHydS-fr11OQjCEuYclbfKLztRf1fPtd3RLbBUDcl98WlpGk4zPCbetYeFk.5YMS5tMHuhBUizscbUQJuEs81a8YolsWcHG5bk8ALqc&dib_tag=se&keywords=howies+cider&qid=1719485199&sprefix=howies+cide%2Caps%2C162&sr=8-5" target="_blank" rel="noopener noreferrer">
+            SHOP
+          </a>
+        </div>
+        <div className='flex items-center gap-4'>
+          <a href="mailto:james@howiescider.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/?size=100&id=63&format=png&color=000000" alt="Email" className="w-6 h-6" />
+          </a>
+          <a href="https://instagram.com/howiescider/" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/?size=100&id=32292&format=png&color=000000" alt="Instagram" className="w-6 h-6" />
+          </a>
+        </div>
       </div>
       <div className="flex flex-col items-center mt-4">
         <img src={Logo} alt="Howie's Cider Logo" className="w-80 mb-8" />
